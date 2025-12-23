26cb218fcccbbaea431dc0f3a00c57a4bf413917
Cvetollina <alla_cvetkova@hotmail.com>
2025-12-23 18:50:52 +0200

    Bricks are falling down!

---
+++ classes.ts
@@ -0,0 +1,75 @@
+class CScreenObject{
+    dom:HTMLDivElement;//Часть сегмента змейки визуально
+    nX: number;//Текущая координата X
+    nY: number;//Текущая координата Y
+    nSize: number;//Размер сегмента в пикселях
+    constructor(x: number, y: number, size: number, fObjectCreator: Function){
+        this.dom = fObjectCreator(x, y, size);//Создаем сегмент по координатам и размеру
+        this.nX = x;
+        this.nY = y;
+        this.nSize = size;
+    }
+
+    Dissapear(){
+        this.dom.remove();
+    }
+
+}
+
+class CCar extends CScreenObject{
+    Move(step: number){
+        const rect = this.dom.getBoundingClientRect();//Получаем текущую позицию слева у элемента относительно окна
+        const oRect = document.getElementById('field')!.getBoundingClientRect();//Получаем размер и позицию границы поля относительно окна
+        if(rect.right + step <= oRect.right && rect.left + step >= oRect.left){
+            this.nX += step;//Меняем виртуальное положение
+            this.dom.style.left = (this.nX-this.nSize/2) + 'px';//Обновляем позицию DOM элемента по X
+        }
+    }
+    constructor(x: number, y: number, size: number){
+        super(x, y, size, ()=>{return CreateObj(x, y, size, "CCar", ravSvg)});
+    }
+}
+
+class CObstacle extends CScreenObject{
+    Fall(step: number) {
+        //const item = this.dom.getBoundingClientRect();
+        //const fieldRect = document.getElementById('field')!.getBoundingClientRect();
+
+        //if (item.bottom + step <= fieldRect.bottom + 100) {
+            this.nY += step;
+            this.dom.style.top = (this.nY - this.nSize / 2) + 'px';
+        //}
+
+    }
+    constructor(x: number, y: number, size: number){
+        super(x, y, size, ()=>{return CreateObj(x, y, size, "Obstacle", ravSvgObst)});
+    }
+    setX(x: number){
+        this.nX = x;
+        this.dom.style.left = (this.nX - this.nSize / 2) + 'px';
+    }
+}
+const aObstacles: CObstacle[]=[];//Создаём пустой массив монет
+
+function createObstacle(x: number){
+    const fieldRect = document.getElementById('field')!.getBoundingClientRect();
+    const y = -100;
+    const size = obstacleSize;
+    
+    const obstacle = new CObstacle(x, y, size);
+    aObstacles.push(obstacle);
+}
+
+class CCoins extends CScreenObject{
+    Fall(step: number){
+        const item = this.dom.getBoundingClientRect();
+        const oItem = document.getElementById('field')!.getBoundingClientRect();//Получаем размер и позицию границы поля относительно окна
+        if(item.bottom + step <= oItem.bottom && item.left + step >= oItem.left){
+            this.nY += step;//Меняем виртуальное положение
+            this.dom.style.top = (this.nY-this.nSize/2) + 'px';//Обновляем позицию DOM элемента по X
+        }
+    }
+    constructor(x: number, y: number, size: number){
+        super(x, y, size, ()=>{return CreateObj(x, y, size, "Coin", "<br>BTC")});
+    }
+}
--- main.ts
+++ main.ts
@@ -1,83 +1,37 @@
-class CScreenObject{
-    dom:HTMLDivElement;//Часть сегмента змейки визуально
-    nX: number;//Текущая координата X
-    nY: number;//Текущая координата Y
-    nSize: number;//Размер сегмента в пикселях
-    constructor(x: number, y: number, size: number, fObjectCreator: Function){
-        this.dom = fObjectCreator(x, y, size);//Создаем сегмент по координатам и размеру
-        this.nX = x;
-        this.nY = y;
-        this.nSize = size;
-    }
-
-    Dissapear(){
-        this.dom.remove();
-    }
-
-}
-
 const ravSvg = `<svg viewBox="0 0 100 100">
                     <rect x="25" y="0" width="50" height="100" rx="10" ry="10" 
                         fill="black" stroke="grey" stroke-width="2" />
                 </svg>`
 
+const ravSvgObst = `<svg viewBox="0 0 100 100">
+                    <rect x="25" y="0" width="50" height="100" rx="10" ry="10" 
+                        fill="orange" stroke="yellow" stroke-width="2" />
+                </svg>`
+
 //const x = Math.random()*(document.documentElement.clientWidth - 200)+100;
 //const y = Math.random()*(document.documentElement.clientHeight - 200)+100;
+const obstacleSize = 20;
+const speed = 30;
+let bObstacleEnough = false;
+setInterval(()=>{
+    const fieldRect = document.body.getBoundingClientRect();
+    if(!bObstacleEnough){
+        const x = Math.random() * (fieldRect.right - fieldRect.left - 100) + fieldRect.left + 50;
+        createObstacle(x);
+    }
 
-class CCar extends CScreenObject{
-    Move(step: number){
-        const rect = this.dom.getBoundingClientRect();//Получаем текущую позицию слева у элемента относительно окна
-        const oRect = document.getElementById('field')!.getBoundingClientRect();//Получаем размер и позицию границы поля относительно окна
-        if(rect.right + step <= oRect.right && rect.left + step >= oRect.left){
-            this.nX += step;//Меняем виртуальное положение
-            this.dom.style.left = (this.nX-this.nSize/2) + 'px';//Обновляем позицию DOM элемента по X
+    for(let i = aObstacles.length - 1; i >= 0; i--){
+        const obstacle = aObstacles[i];
+        obstacle.Fall(speed);
+
+        if(obstacle.nY > fieldRect.bottom){
+            bObstacleEnough = true;
+            obstacle.Fall(-fieldRect.height-200);
+            obstacle.setX(Math.random() * (fieldRect.right - fieldRect.left - 100) + fieldRect.left + 50);
         }
     }
-    constructor(x: number, y: number, size: number){
-        super(x, y, size, ()=>{return CreateObj(x, y, size, "CCar", ravSvg)});
-    }
-}
 
-class CObstacle extends CScreenObject{
-    Fall(step: number) {
-        const item = this.dom.getBoundingClientRect();
-        const fieldRect = document.getElementById('field')!.getBoundingClientRect();
-
-        if (item.bottom + step <= fieldRect.bottom + 100) {
-            this.nY += step;
-            this.dom.style.top = (this.nY - this.nSize / 2) + 'px';
-        }
-
-    }
-    constructor(x: number, y: number, size: number){
-        super(x, y, size, ()=>{return CreateObj(x, y, size, "OBSTACLE", "ravSvg")});
-    }
-}
-const aObstacles: CObstacle[]=[];//Создаём пустой массив монет
-
-function createObstacle(){
-    const fieldRect = document.getElementById('field')!.getBoundingClientRect();
-    const x = Math.random() * (fieldRect.right - fieldRect.left - 100) + fieldRect.left + 50;
-    const y = -100;
-    const size = 50;
-    
-    const obstacle = new CObstacle(x, y, size);
-    //aObstacles.push(new CObstacle);
-}
-
-class CCoins extends CScreenObject{
-    Fall(step: number){
-        const item = this.dom.getBoundingClientRect();
-        const oItem = document.getElementById('field')!.getBoundingClientRect();//Получаем размер и позицию границы поля относительно окна
-        if(item.bottom + step <= oItem.bottom && item.left + step >= oItem.left){
-            this.nY += step;//Меняем виртуальное положение
-            this.dom.style.top = (this.nY-this.nSize/2) + 'px';//Обновляем позицию DOM элемента по X
-        }
-    }
-    constructor(x: number, y: number, size: number){
-        super(x, y, size, ()=>{return CreateObj(x, y, size, "Coin", "<br>BTC")});
-    }
-}
+}, 100)
 
 const aCoins: CCoins[]=[];//Создаём пустой массив монет
 
